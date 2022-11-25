e2f7276e7ad5ec6ce0eb8316da9a60c15c191702
sameh-dh <samehdh44@gmail.com>
2022-11-25 14:56:29 +0100

    set up

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const app =express()
+app.use(express.json())
+const PORT =4000
+app.listen(PORT,()=>{
+    console.log(`server connected to localhost:${PORT}`)
+})
