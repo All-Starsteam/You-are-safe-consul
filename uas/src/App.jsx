65b38bda0e97561494ae609345d2d90f3c5ebb9b
sameh-dh <samehdh44@gmail.com>
2022-11-26 02:40:16 +0100

    nav bar design done

--- uas/src/App.jsx
+++ uas/src/App.jsx
@@ -1,43 +1,133 @@
 import React from "react";
-import {useEffect,useState} from "react"
+import { useEffect, useState } from "react";
 import AllComplains from "./components/AllComplains.jsx";
 import exampleData from "./dummydata";
 import OneComplain from "./components/OneComplain.jsx";
 import CreateComplain from "./components/CreateComplain.jsx";
+import { AiOutlineMenu, AiOutlineClose,AiOutlineSearch } from "react-icons/ai";
+import quotes from "./images/quotes1.png";
 import axios from "axios";
-import './index.css'
+import "./index.css";
 
-let  App=()=> {
-  const [view, setView] = useState("AllComplains");
-  const [data, setData] = useState(exampleData);
-//  useEffect(() => {
-//    axios.get("").then((res) => {
-//      setData(res.data);
-//    });
-//  }, []);
- const changeView=(newview)=>{
-  setView(newview);
- }
-   const addComplain=(complain)=> {
-      console.log(complain);
-     axios.post('',complain).then(res=>console.log('passed')).catch(err=>console.log(err))
+let App = () => {
+  // show and hide menu
+  var [menu, setMenu] = useState(true);
+  var toggleMenu = () => {
+    if (menu === true) {
+      return (
+        <div className="navs">
+          <div className="nav_menu" id="nav-menu">
+            <div className="nav_close" id="nav-close">
+              <AiOutlineClose />
+            </div>
+            <ul className="nav_list">
+              <li className="nav_item">
+                <div className="nav_linksignIn">sign in </div>
+              </li>
+              <li className="nav_item">
+                <div className="nav_linksignUp"> Register </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="nav_menu show" id="nav-menu">
+          <div className="nav_close" id="nav-close">
+            <AiOutlineClose
+              onClick={() => {
+                setMenu(!menu);
+              }}
+            />
+          </div>
+          <ul className="nav_list">
+            <li className="nav_item">
+              <div href="#" className="nav_link"></div>
+            </li>
+            <li className="nav_item">
+              <div href="#" className="nav_link"></div>
+            </li>
+            <li className="nav_item">
+              <div href="#" className="nav_link"></div>
+            </li>
+            <li className="nav_item">
+              <div href="#" className="nav_link"></div>
+            </li>
+          </ul>
+        </div>
+      );
     }
-  if (view === "AllComplains") {
-    return <AllComplains data={data} changeView={changeView}/>;
-  } else if (view === "OneComplain") {
-    return <OneComplain  />;
-  } 
-  else if (view === "createComplain") {
-    return <CreateComplain addComplain={addComplain} />;
-  }
+  };
+  return (
+    <div>
+      <head>
+        <title> You are safe </title>
+      </head>
+      {/* first navbar */}
+      <header className="l-header">
+        <nav className="nav bd-grid">
+          <div className="first_nav">
+            <div className="consul-header">
+              {" "}
+              <img className="quote-header" src={quotes} /> Consul
+            </div>
+          </div>
+          <div className="nav_toggle" id="nav-toggle">
+            <AiOutlineMenu
+              onClick={() => {
+                setMenu(!menu);
+              }}
+            />
+          </div>
+          {toggleMenu()}
+        </nav>
+      </header>
 
-}
+      <main></main>
+      {/* second navabar */}
+      <header className="l-header2">
+        <nav className="nav bd-grid2">
+          <ul className="nav_list2">
+            <li className="nav_item2">
+              <div className="nav_link2">Debates</div>
+            </li>
+            <li className="nav_item2">
+              <div className="nav_link2">Proposals </div>
+            </li>
+            <li className="nav_item2">
+              <div className="nav_link2">Voting</div>
+            </li>
+            <li className="nav_item2">
+              <div className="nav_link2"> collaborative legislation </div>
+            </li>
+            <li className="nav_item2">
+              <div className="nav_link2"> Participatory budgeting </div>
+            </li>
+            <li className="nav_item2">
+              <div className="nav_link2"> you are safe</div>
+            </li>
+            <li className="nav_item2">
+              <div className="nav_link2"> SDG </div>
+            </li>
+            <li className="nav_item2">
+              <div className="nav_link2"> Help</div>
+            </li>
+            <div class="wrap">
+   <div class="search">
+      <input type="text" class="searchTerm" placeholder="search" />
+      <button type="submit" class="searchButton">
+       <AiOutlineSearch />
+     </button>
+   </div>
+</div>
+          </ul>
+        </nav>
+      </header>
+
+      <footer></footer>
+    </div>
+  );
+};
 
 export default App;
-
-
-
-
-
-
-
---
+++ uas/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+var Home = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Home;
